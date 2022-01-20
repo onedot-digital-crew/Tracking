60ca9f9447e5d222318e9525f1dead8eb1074beb
kdenckert94 <k.denckert@one-dot.de>
2022-01-20 12:51:15 +0100

    fix ODOD

--- src/Providers/GtmProviderHead.php
+++ src/Providers/GtmProviderHead.php
@@ -8,6 +8,6 @@ class GtmProviderHead
 {
     public function call(Twig $twig)
     {
-        return $twig->render('ODODTracking::GtmHead');
+        return $twig->render('ODTracking::GtmHead');
     }
 }
